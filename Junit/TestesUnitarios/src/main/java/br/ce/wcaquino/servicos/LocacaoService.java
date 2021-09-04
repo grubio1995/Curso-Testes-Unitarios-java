070eda2de0451fe23a47019cc0fdc0a1ec32f0fd
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-09-04 15:50:23 -0300

    Organização dos arquivos de teste

--- Junit/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ Junit/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -1,17 +1,12 @@
 package br.ce.wcaquino.servicos;
 
 import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Date;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -33,21 +28,5 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	@Test
-	public void teste() { 
 
-		// cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuário 1");
-		Filme filme = new Filme("Filme 1", 2, 5.0);
-		
-		//acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		
-		//verificação
-		Assertions.assertTrue(locacao.getValor() == 5.0);
-		Assertions.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(),new Date()));
-		Assertions.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(),DataUtils.obterDataComDiferencaDias(1)));
-
-	}
 }
